478381acd8987c7b640226c003ae59733b0510a1
calix <jykcalix@gmail.com>
2022-10-10 00:00:31 +0900

    Add Level1 files

--- Level1/문자열 다루기 기본.py
+++ Level1/문자열 다루기 기본.py
@@ -1,11 +1,12 @@
 def solution(s):
-    try:
-        if len(s)==4 or len(s)==6:
-            a = int(s)
-            return True
-        else:
-            return False
-    except:
-        return False
+    # try:
+    #     if len(s)==4 or len(s)==6:
+    #         a = int(s)
+    #         return True
+    #     else:
+    #         return False
+    # except:
+    #     return False
     
 # Comment: isdigit() 함수 사용 가능
+    return s.isdigit() and (len(s)==4 or len(s)==6)
