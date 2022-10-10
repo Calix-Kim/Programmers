d41aef3457a8bc73a3ceb54f18e6624d73d8de28
calix <jykcalix@gmail.com>
2022-10-11 00:10:16 +0900

    Add Level1 files

--- Level1/문자열 다루기 기본.py
+++ Level1/문자열 다루기 기본.py
@@ -2,7 +2,7 @@ def solution(s):
     # try:
     #     if len(s)==4 or len(s)==6:
     #         a = int(s)
-    #         return True
+    #         return  
     #     else:
     #         return False
     # except:
